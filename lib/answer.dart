d5f49e790a0ae9adaebae4f6e3731842e95346c8
Karel Parkkola <karel.parkkola@gmail.com>
2022-06-21 00:10:04 +0300

    More progress

---
+++ lib/answer.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class Answer extends StatelessWidget {
+  final String buttonText;
+  final Function selectHandler;
+
+  const Answer(
+      {Key? key, required this.buttonText, required this.selectHandler})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: ElevatedButton(
+        onPressed: selectHandler,
+        style: ElevatedButton.styleFrom(
+          primary: Colors.amber,
+        ),
+        child: Text(buttonText),
+      ),
+    );
+  }
+}
